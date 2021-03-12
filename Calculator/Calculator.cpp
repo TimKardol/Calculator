83136fcb0839ab5e5a052de3454f069fa2a0e819
TimKardol <tnmkardol@outlook.com>
2021-03-12 01:03:21 +0100

    Add public Calculator class

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -2,13 +2,16 @@
 //
 
 #include <iostream>
+#include "Calculator.h"
 
 using namespace std;
 
 int main()
 {
     cout << "Calculator Console Application" << endl << endl; 
-    cout << "Please enter the operation to perform.
+    cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+        << endl;
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
